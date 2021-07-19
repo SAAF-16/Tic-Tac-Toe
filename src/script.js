ae9b2bcecef27769ce056359b1c8990a53b8c62b
SAAF-16 <samsnow16@gmail.com>
2021-07-19 21:06:22 +0200

    added comments

--- src/script.js
+++ src/script.js
@@ -5,14 +5,14 @@ const game_board = (() => {
     const set_box = (x, y, symbol) => {
         board[x][y] = symbol;
     };
-
     const get_box = (x, y) => {
         return board[x][y];
     };
     const get_board = () => {
         return board;
     };
-    function converter(x) {
+    //converts an index x to board coordinates [x,y]
+    function converter(x) {     
         for (let i = 0; i < 3; i++) {
             for (let j = 0; j < 3; j++) {
                 if (x == 0) {
@@ -23,7 +23,7 @@ const game_board = (() => {
         }
         return false;
     }
-    function clear() {
+    function clearBoard() {
         board = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
     }
     function get_validMoves() {
@@ -40,7 +40,7 @@ const game_board = (() => {
     return {
         get_validMoves,
         get_box,
-        clear,
+        clearBoard,
         converter,
         set_box,
         get_board,
@@ -50,15 +50,16 @@ const game_board = (() => {
 
 
 const player = (type, name) => {
-    let selected = 0;
+    //represents the selected cell on the board
+    let selected = 0;       
     let active = true;
+
     const set_selected = (x) => {
         selected = x;
     };
     const get_selected = () => {
         return selected;
     };
-
     const set_move = (move) => {
         if (type == "Hard AI") {
             [x, y] = game_manager.computerMove(10);
@@ -88,11 +89,9 @@ const player = (type, name) => {
 };
 
 const visual_manager = (() => {
-    let started=false;
-    const restart = document.getElementById("restart");
-    const playerOne_selector = document.getElementById("playerOne_selector");
-    const playerTwo_selector = document.getElementById("playerTwo_selector");
-    const dropdownLists = document.querySelectorAll("select");
+    let started = false;
+    const playerOne_selector = document.querySelector("#playerOne_selector");
+    const playerTwo_selector = document.querySelector("#playerTwo_selector");
     const boxes = document.querySelectorAll(".box");
     const div_game_board = document.querySelector("#game_board");
     const playerOne_name = document.querySelector("#playerOne_name");
@@ -101,61 +100,50 @@ const visual_manager = (() => {
     const winner_text = document.querySelector("#winner_text");
 
     newGame_button.addEventListener('click', () => {
-        if(playerOne_selector.value!="Human")playerOne_name.value="Computer"
-        if(playerTwo_selector.value!="Human")playerTwo_name.value="Computer"
-        if(playerOne_name.value=="")playerOne_name.value="Anonymus"
-        if(playerTwo_name.value=="")playerTwo_name.value="Anonymus"
-        newGame_button.disabled = true
-        playerOne_selector.disabled = true
-        playerTwo_selector.disabled = true
+        if (playerOne_selector.value != "Human") playerOne_name.value = "Computer";
+        if (playerTwo_selector.value != "Human") playerTwo_name.value = "Computer";
+        if (playerOne_name.value == "") playerOne_name.value = "Anonymus";
+        if (playerTwo_name.value == "") playerTwo_name.value = "Anonymus";
+        newGame_button.disabled = true;
+        playerOne_selector.disabled = true;
+        playerTwo_selector.disabled = true;
         newGame_button.classList.add("invisible");
         playerOne_name.classList.add("names");
         playerTwo_name.classList.add("names");
-        playerOne_name.disabled = true
-        playerTwo_name.disabled = true
-        if (!started){started=true
+        playerOne_name.disabled = true;
+        playerTwo_name.disabled = true;
+        //the listener is added only one time at the beginning
+        if (!started) {     
+            started = true;
             div_game_board.addEventListener('click', (e) => {
                 game_manager.humanMove(e.target.id);
-            });}
-            winner_text.textContent = "";
-            game_board.clear();
-            update_board();
+            });
+        }
+        winner_text.textContent = "";
+        game_board.clearBoard();
+        update_board();
         game_manager.start(playerOne_selector.value, playerTwo_selector.value, playerOne_name.value, playerTwo_name.value);
-
-        // dropdownLists.forEach((dropdown) => {
-        //     dropdown.addEventListener("change",newGame);
-        // });
     });
 
     function endGame(x) {
         winner_text.textContent = x;
-        newGame_button.disabled = false
-        playerOne_selector.disabled=false
-        playerTwo_selector.disabled = false
-        playerOne_name.disabled = false
-        playerTwo_name.disabled = false
-        if(playerOne_name.value=="Anonymus"||playerOne_name.value=="Computer")playerOne_name.value=""
-        if(playerTwo_name.value=="Anonymus"||playerTwo_name.value=="Computer")playerTwo_name.value=""
+        newGame_button.disabled = false;
+        playerOne_selector.disabled = false;
+        playerTwo_selector.disabled = false;
+        playerOne_name.disabled = false;
+        playerTwo_name.disabled = false;
+        if (playerOne_name.value == "Anonymus" || playerOne_name.value == "Computer") playerOne_name.value = "";
+        if (playerTwo_name.value == "Anonymus" || playerTwo_name.value == "Computer") playerTwo_name.value = "";
         newGame_button.classList.remove("invisible");
         playerOne_name.classList.remove("names");
         playerTwo_name.classList.remove("names");
 
     }
 
-    function newGame() {
-        winner_text.textContent = "";
-
-
-        game_manager.start(playerOne_selector.value, playerTwo_selector.value, playerOne_name.value, playerTwo_name.value);
-
-    };
-
     function update_board() {
         board = game_board.get_board();
         boxes.forEach((box) => {
-
             [x, y] = game_board.converter(box.id);
-
             if (board[x][y] == 0) { box.textContent = ""; box.classList.remove("box_marked"); }
             else if (board[x][y] == 1) { box.textContent = "X"; box.classList.add("box_marked"); }
             else if (board[x][y] == -1) { box.textContent = "O"; box.classList.add("box_marked"); }
@@ -174,14 +162,15 @@ const game_manager = (() => {
     let player_two;
     let round = -1;
     let active_player;
-    let dummy_player = player("Human", "");
+    //player used in the minimax simulations
+    let dummy_player = player("Human", "");     
     let dummy_moves = [];
 
     function start(typeOne, typeTwo, nameOne, nameTwo) {
         player_one = player(typeOne, nameOne);
         player_two = player(typeTwo, nameTwo);
         round = 0;
-        if (player_one.type!="Human") play();
+        if (player_one.type != "Human") play();
         else player_two.active = false;
     }
 
@@ -200,14 +189,14 @@ const game_manager = (() => {
         }
         visual_manager.update_board();
         if (check_end()) return false;
-        if (player_one.type!="Human" && player_two.type!="Human")
+        if (player_one.type != "Human" && player_two.type != "Human")
             setTimeout(function () { play(); }, 300);
         else if (player_one.type == "Human" && player_two.type == "Human") {
 
             player_one.active = !player_one.active;
             player_two.active = !player_two.active;
         }
-        else if (active_player.type!="Human") return false;
+        else if (active_player.type != "Human") return false;
         else if (active_player.type == "Human")
             setTimeout(function () { play(); }, 300);
 
@@ -215,11 +204,11 @@ const game_manager = (() => {
 
     }
 
-    function player_move(player, currentS, move) {
+    function player_move(player, currentSymbol, move) {
         do {
             [x, y] = player.set_move(move);
         } while (!check_playable(x, y));
-        game_board.set_box(x, y, currentS);
+        game_board.set_box(x, y, currentSymbol);
         round++;
 
     }
@@ -228,14 +217,14 @@ const game_manager = (() => {
         round % 2 == 0 ? active_player = player_one : active_player = player_two;
         if (!active_player.active) return false;
         active_player.set_selected(game_board.converter(id));
-        if (active_player.get_selected == 0) return false;
+        if (active_player.get_selected() == 0) return false;
         if (!check_playable(...active_player.get_selected())) return false;
         play(active_player.get_selected());
     }
 
     function computerMove(difficulty) {
-        let { dummy_moves } = minimax(0,difficulty);
-        // Pick a random move when there are choices:
+        let { dummy_moves } = minimax(0, difficulty);
+        // Pick a random move when there are choices
         return dummy_moves[Math.floor(Math.random() * dummy_moves.length)];
     }
 
@@ -246,20 +235,20 @@ const game_manager = (() => {
         }
         return false;
     }
-
+    // if called with test it's a minimax simulation and it returns -10 for win and 0 for draw
     function check_end(test) {
         if (win_combo()) {
-            if (test != undefined) return -10;
+            if (test != undefined) return -10;  
             if (round > 9) return true;
             if (round % 2 == 1) visual_manager.endGame(player_one.name + " won!");
-            else visual_manager.endGame(player_two.name + " won");
+            else visual_manager.endGame(player_two.name + " won!");
             round = 10;
             return true;
         } else if (round == 9) {
             if (test != undefined) {
                 return 0;
             }
-            visual_manager.endGame("It's a draw");
+            visual_manager.endGame("It's a draw!");
             round = 10;
             return true;
         }
@@ -298,6 +287,7 @@ const game_manager = (() => {
         return false;
     }
 
+    //reverts minimax simulation moves
     function takeBack() {
         if (dummy_moves.length === 0) return false; // cannot undo
         [x, y] = dummy_moves.pop();
@@ -305,15 +295,18 @@ const game_manager = (() => {
         return true;
     }
 
-    function minimax(depth,difficulty) {
-        depth++
-        if(depth>difficulty)return 0
+    //minimax algorith to find next move, depth how many times the function was called
+    function minimax(depth, difficulty) {
+        depth++;
+        if (depth > difficulty) return 0;
         let simbol;
         dummy_player;
         round % 2 == 0 ? simbol = 1 : simbol = -1;
         let ende = check_end(1);
-        if (ende) return { value: ende };       //win situation
-        if (ende == 0) return { value: ende };    //draw situation
+        //win situation
+        if (ende) return { value: ende }; 
+        //draw situation      
+        if (ende == 0) return { value: ende };    
 
         let best = { value: -Infinity };
         for (let move of game_board.get_validMoves()) {
@@ -322,7 +315,7 @@ const game_manager = (() => {
             game_board.get_board()[x][y] = simbol;
             dummy_moves.push(move);
             ++round;
-            let { value } = minimax(depth,difficulty);
+            let { value } = minimax(depth, difficulty);
             takeBack();
             --round;
             // Reduce magnitude of value (so shorter paths to wins are prioritised) and negate it
@@ -330,7 +323,8 @@ const game_manager = (() => {
             if (value >= best.value) {
                 if (value != 10 && value < 10) {
                     if (value > best.value) best = { value, dummy_moves: [] };
-                    best.dummy_moves.push(move); // keep track of equally valued moves
+                    // keep track of equally valued moves
+                    best.dummy_moves.push(move); 
                 }
             }
         }
